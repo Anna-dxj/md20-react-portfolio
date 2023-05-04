724686f18c9543691dfa7bae2716c79488899bc3
Xiaojia <annacl1822@gmail.com>
2023-05-03 23:22:24 -0700

    basic configuration & navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Here we are using object destructuring assignment to pluck off our variables from the props object
+// We assign them to their own variable names
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <h1 className="navbar-brand">Anna Langford</h1>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
