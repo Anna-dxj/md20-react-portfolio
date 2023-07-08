9ebba24c4746a9143c57b6fe8624d9435908e32b
Xiaojia <annacl1822@gmail.com>
2023-07-08 16:03:54 -0700

    update resume

--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -1,42 +1,94 @@
 import React from 'react'
 
-export default function ResumeCard({title, location, time, ach, gpa, technologies, description}){
+export default function ResumeCard({type, dates, school, location, description, ach, title, employer, org, skills}){
+    const splitSkills = (skills) => {
+        console.log(skills)
+        // const skillList = skills.split("; ")
+        // console.log(skillList)
+    }
+    splitSkills(skills)
     return (
-        <div className="custom-card my-2 col-sm-12">
-            <h3 className="card-title custom-card-header">{title}</h3>
-            <div className="py-2 px-3">
-              {(ach) ? (
-                  <p className="card-text">{ach}</p>
-              ) : ('')}
-              <p className="card-text">{location}, {time}</p>
-              {(gpa) ? (
-                <p className="card-text">GPA: {gpa}/4.00</p>
-              ) : ('')}
-              {(technologies) ? (
-                <div>
-                    <p clasName="card-text">{technologies.split('; ').slice(-1).toString()}</p>
-                    <div className="row d-flex flex-row justify-content-center align-items-start">
-                        <ul className="list-group col-sm-6 col-md-12 px-2">
-                            {technologies.split('; ').slice(0, 4).map((item) => {
-                                return <li className="list-group-item">{item}</li>
-                            })}
-                        </ul>
-                        <ul className="list-group col-sm-6 col-md-12 py-1 px-2">
-                            {technologies.split('; ').slice(5, 10).map((item) => {
-                                return <li className="list-group-item">{item}</li>
-                            })}
+        <div>
+            {type === 'education' && (
+                <div className="custom-card my-2 col-sm-12">
+                    <h3 className="card-title custom-card-header">{ach}</h3>
+                    <div className="py-2 px-3">
+                        <div className="d-flex justify-content-between">
+                            <p>
+                                <span className="custom-text">{school}</span>, {location}
+                            </p>
+                            <p>{dates}</p>
+                        </div>
+                        <p>{description}</p>
+                    </div>
+                </div>
+            )}
+            {type === 'experience' && (
+                <div className="custom-card my-2 col-sm-12">
+                    <h3 className="card-title custom-card-header">{title}</h3>
+                    <div className="py-2 px-3">
+                        <div className="d-flex justify-content-between">
+                            <p> 
+                                <span className="custom-text">{employer}</span>, {location}
+                            </p>
+                            <p>{dates}</p>
+                        </div>
+                        <p>{description}</p>
+                        <ul className='list-group'>
+                            {ach && ach.split(". ").map((key) => (
+                                <li className='list-group-item'>{key}</li>
+                            ))}
                         </ul>
                     </div>
                 </div>
-              ) : ('')}
-              {(description) ? (
-                <ul className="list-group">
-                    {description.split('. ').map((item) => {
-                        return <li className="list-group-item">{item}</li>
-                    })}
-                </ul>
-              ) : ('')}
-            </div>
-          </div>
+            )}
+            {type === 'leadership' && (
+                <div className="custom-card my-2 col-sm-12">
+                    <h3 className="card-title custom-card-header">{title}</h3>
+                    <div className="py-2 px-3">
+                        <div className="d-flex justify-content-between">
+                            <p><span className="custom-text">{org}</span>, {location}</p>
+                            <p>{dates}</p>
+                        </div>
+                        <ul className='list-group'>
+                            {ach && ach.split(". ").map((key) => (
+                                <li className='list-group-item'>{key}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+            {type === 'skills' && (
+                <div className="custom-card my-2 col-sm-12">
+                    <h3 className="card-title custom-card-header">{title}</h3>
+                    <div className="py-2 px-3">
+                            {skills.split("; ").length > 10 ? (
+                                <div className="row d-fle flex-row justify-content-center align-items-start">
+                                    <ul className="list-group col-md-6 col-sm-12 px-2">
+                                        {
+                                            skills.split("; ").slice(0, 8).map((skill) => (
+                                                <li className="list-group-item">{skill}</li>
+                                            )
+                                        )}
+                                    </ul>
+                                    <ul className="list-group col-md-6 col-sm-12 py-1 px-2">
+                                        {
+                                            skills.split("; ").slice(8, 15).map((skill) => (
+                                                <li className="list-group-item">{skill}</li>
+                                            )
+                                        )}
+                                    </ul>
+                                </div>
+                            ) : (
+                                <ul className='list-group'>
+                                    {skills.split("; ").map((skill) => (
+                                        <li className="list-group-item">{skill}</li>
+                                    ))}
+                                </ul>
+                            )}
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -17,58 +17,82 @@ export default function Home() {
       </h1>
       <div className="row d-flex flex-row justify-content-center">
         <div className="custom-card-background container py-2 my-2 px-3 shadow-lg col-sm-12 col-md-5">
-          <h2 className="card-title">Education</h2>
-          <div className="card-body align-items-center d-flex flex-column justify-content-around">
-            <ResumeCard 
-              title="Rice University"
-              location="Houston, Texas"
-              time="Auguest 2018 - May 2022"
-              ach="Bachelor of Arts in Linguistics"
-              gpa="3.61"
+          <h2 className="card-title">Technical Skills</h2>
+          <div className="card-body d-flex flex-column justify-content-around">
+            <ResumeCard
+              type="skills"
+              title="Software/Platforms"
+              skills="GitHub Workflow; jQuery; CSS & UI Libraries; Node.js & Express.js; MySQL & Sequelize; MongoDB & Mongoose; RESTful API & GraphQL; React; Object-Oriented Programming; Functional Programming; Test-driven Development; Progressive Web Applications; MERN Stack; Responsive Design; Separation of Concerns (MVC, Client-Server)"
             />
-            <ResumeCard 
-              title="UCLA Extension Full-Stack Coding Bootcamp"
-              location="Virtual"
-              time="November 2022 - May 2023"
-              ach="Certificate"
-              technologies="HTML; CSS and responsive design; CSS Libraries (Bootstrap, Materialize.css); JavaScript and jQuery; React; RESTful APIs and GraphQL; Node; Express; MySQL and Sequelize; MongoDB and Mongoose; Studied and learned modern front-end and backend proficiencies:"
-
-            />
-          </div>
-        </div>
-        <div className="custom-card-background container py-2 my-2 px-3 shadow-lg col-sm-12 col-md-5">
-          <h2 className="card-title">Leadership and Involvement</h2>
-          <div className="card-body align-items-center d-flex flex-column justify-content-around">
-            <ResumeCard 
-              title="Founder and President of Rice Chinsese Dance Club"
-              location="Houston, Texas"
-              time="Fall 2021 - May 2022"
-              description="Coordinated with other campus clubs for performances and establish ongoing relationships for future events and performances. Founded and managed club, overseeing all aspects of operations, including organizing and teaching practices, recruiting members, and collaborating with the treasurer to secure funding"
-            />
-            <ResumeCard 
-              title="Director for BASYK Dance Club"
-              location="Houston, Texas"
-              time="Fall 2018 - May 2022"
-              description="Managed my own dancers and acted as middleman when communicating with the board to schedule practices and filming and to relay instructions or concerns. Participated in other directors' projects, and advised newer directors if requested"
+            <ResumeCard
+              type="skills"
+              title="Software Languages"
+              skills="HTML; CSS; JavaScript; SQL; GraphQL"
             />
           </div>
         </div>
         <div className="custom-card-background container py-2 my-2 px-3 shadow-lg col-sm-12 col-md-5">
           <h2 className="card-title">Experience</h2>
-          <div className="card-body align-items-center d-flex flex-column justify-content-around">
-            <ResumeCard 
-              title="Rice University Linguistics Department"
-              location="Houston, Texas"
-              time="Fall 2020, Fall 2021" 
-              ach="Teaching Assistant"
-              description="Composed the midterm and guidelines for final papers and other assignments with the professor for Language and Society. Graded assignments and coursework and provided feedback for Language and Society"
+          <div className="card-body d-flex flex-column justify-content-around">
+          <ResumeCard 
+              type="experience"
+              title="Private Math Tutor"
+              employer="Self-Employed"
+              location="Scottsdale, Arizona"
+              dates="2022 - ongoing"
+              description="Provided personalized assitance to students struggling in Math, aiding in developing study skills, test-taking skills, and general problem-solving skills."
+              ach="Communicated with parents on students' progress, difficulty areas, and plans to improve grade. Adapted tutoring session to accomodate different learning styles, strengths, and weaknesses"
             />
             <ResumeCard 
-              title="Self-Employed"
-              location="Scottsdale, Arizona"
-              time="Fall 2022 - ongoing"
-              ach="Private Math Tutor"
-              description="Provided personalized assistance to students struggling in Math, helping them develop study skills and re-explaining material as needed"
+              type="experience"
+              title="Teaching Assistant"
+              employer="Rice University Linguistics Department"
+              location="Houston, Texas"
+              dates="2020, 2021" 
+              description="Fulfilled Teaching Assistant responsibilities for Language and Society wile managing a full course load."
+              ach="Composed the midterm and guidelines for final papers and other assignments with the professor for Language and Society. Graded assignments and coursework and provided feedback for Language and Society"
+            />
+          </div>
+        </div>
+        <div className="custom-card-background container py-2 my-2 px-3 shadow-lg col-sm-12 col-md-5">
+          <h2 className="card-title">Leadership and Involvement</h2>
+          <div className="card-body d-flex flex-column justify-content-around">
+            <ResumeCard 
+              type="leadership"
+              title="Founder and President of Rice Chinsese Dance Club"
+              org="Rice University"
+              location="Houston, Texas"
+              dates="2021 - 2022"
+              ach="Communicated with other campus clubs for events and establish ongoing relationships for future events. Founded and managed club, overseeing all aspects of operations, including organizing and teaching practices, recruiting members, and collaborating with board members to ensure smooth operations"
+            />
+            <ResumeCard 
+              type="leadership"
+              title="Director for BASYK Dance Club"
+              org="Rice University"
+              location="Houston, Texas"
+              dates="2018 - 2022"
+              ach="Managed my own dancers and acted as a liason between dancers and the board to communicate instructions, concerns, and scheduling for practices and filming. Participated in other directors' projects, acted as a mentor to and advised new directors"
+            />
+          </div>
+        </div>
+        <div className="custom-card-background container py-2 my-2 px-3 shadow-lg col-sm-12 col-md-5">
+          <h2 className="card-title">Education</h2>
+          <div className="card-body d-flex flex-column flex-fill justify-content-around">
+            <ResumeCard 
+              type = "education"
+              ach="Full Stack Web Development Certificate"
+              dates="2022 - 2023"
+              school="UCLA"
+              location="Los Angeles, California"
+              description="A 24-week intensive program focused on gaining skills in modern, JavaScript-based web technologies and development."
+            />
+            <ResumeCard 
+              type = "education"
+              ach="Bachelor of Arts in Linguistics"
+              dates="2018 - 2022"
+              school="Rice University"
+              location="Houston, Texas"
+              description="GPA: 3.61/4.00"
             />
           </div>
         </div>
